3b4615891a4126e6a1eaa839cc3b8e1029ed48d6
yonatan_betito <yonatanbetito@gmail.com>
2025-07-14 16:15:58 +0300

    deleted

--- DB/db.js
+++
@@ -1,8 +0,0 @@
-import mysql from "mysql2/promise";
-
-export const pool = mysql.createPool({
-  host: process.env.DB_host || "localhost",
-  user: process.env.DB_user || "root",
-  password: "",
-  database: "classicmodels",
-});
